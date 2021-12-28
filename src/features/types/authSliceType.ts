72849b5586b915a3b01587672875086a0882d2ac
t-nagasaka <66255777+t-nagasaka@users.noreply.github.com>
2021-12-29 05:19:45 +0900

    create_develop_branch

---
+++ src/features/types/authSliceType.ts
@@ -0,0 +1,21 @@
+// 型情報をBlobからエクステンド
+export interface File extends Blob {
+  readonly lastModified: number;
+  readonly name: string;
+}
+
+// authSlice.tsで使用する/
+export interface PROPS_AUTHEN {
+  email: string;
+  password: string;
+}
+
+export interface PROPS_PROFILE {
+  id: number;
+  nickName: string;
+  img: File | null;
+}
+
+export interface PROPS_NICKNAME {
+  nickName: string;
+}
